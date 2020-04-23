1b4130b993f329bbb803c316bf573387bdb3a335
Jobson Gilberto Barros Amorim <jobsonita@gmail.com>
2020-04-22 22:07:53 -0300

    sessão02-aula03 - Construindo Aplicação - Validando a data

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,17 +1,35 @@
 import { Router } from 'express'
 import { uuid } from 'uuidv4'
+import { isEqual, parseISO, startOfHour } from 'date-fns'
 
 const router = Router()
 
-const appointments = []
+interface Appointment {
+  id: string
+  provider: string
+  date: Date
+}
+
+const appointments: Appointment[] = []
 
 router.post('/', (req, res) => {
   const { provider, date } = req.body
 
+  const parsedDate = startOfHour(parseISO(date))
+  const findAppointmentInSameDate = appointments.find((appointment) =>
+    isEqual(parsedDate, appointment.date)
+  )
+
+  if (findAppointmentInSameDate) {
+    return res
+      .status(400)
+      .json({ message: "There's another appointment booked at that time" })
+  }
+
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: parsedDate,
   }
 
   appointments.push(appointment)
